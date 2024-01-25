a0bd6e14d778f17677ea7fac8959150c42489adb
leoleoyuuki <rm550373@fiap.com.br>
2024-01-25 19:14:30 -0300

    main page mobile finish

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,7 +4,7 @@ import { FaArrowRightLong } from "react-icons/fa6";
 import { FaDiscord } from "react-icons/fa";
 import { IoIosMail } from "react-icons/io";
 
-export default function home() {
+export default function Home() {
   const benefits = [
     {
       id: 1,
@@ -35,14 +35,14 @@ export default function home() {
       image: "/img/mais-estavel.jpg",
     },
   ];
+
   return (
     <div className="bg-[#302C42]">
+      {/* Seção 1 */}
       <section className="w-4/5 m-auto">
-        {" "}
-        {/*section 1*/}
-        <div className="container-sec1 flex justify-between">
+        <div className="container-sec1 flex flex-col-reverse items-center md:flex-row justify-between">
           <div className="leftsec1">
-            <div className="texts text-4xl font-semibold pt-24">
+            <div className="texts text-4xl font-semibold pt-10 md:pt-24 ">
               <h1>
                 <span className="t1">Ganhe</span> uma Experiência de jogo
               </h1>
@@ -51,23 +51,28 @@ export default function home() {
               </h1>
             </div>
             <div className="subtitle">
-              <p className="text-white w-[54rem] py-16">
-              Você já imaginou experimentar seus jogos favoritos sem interrupções, gráficos fluidos e um desempenho impecável? Agora, isso é possível com o BoostSync. Não se contente com uma experiência de jogo comum; eleve o nível com a otimização avançada que o BoostSync proporciona.
-              Para gamers fanáticos ou qualquer pessoa que queira turbinar seu PC para games, BoostSync é a solução ideal.
+              <p className="text-white w-[54rem] py-8 md:py-16">
+                Você já imaginou experimentar seus jogos favoritos sem
+                interrupções, gráficos fluidos e um desempenho impecável? Agora,
+                isso é possível com o BoostSync. Não se contente com uma
+                experiência de jogo comum; eleve o nível com a otimização
+                avançada que o BoostSync proporciona. Para gamers fanáticos ou
+                qualquer pessoa que queira turbinar seu PC para games, BoostSync
+                é a solução ideal.
               </p>
             </div>
-            <div className="flex items-center gap-7">
+            <div className="flex flex-col md:flex-row items-center gap-7">
               <Link
                 href="/checkout"
                 className="cta text-[#343045] px-6 py-4 text-xs font-bold"
               >
                 GARANTA SEU DESEMPENHO
               </Link>
-              <FaArrowRightLong size={36} color="#c0b7e8" />
+              <FaArrowRightLong className="seta" size={36} color="#c0b7e8" />
             </div>
           </div>
           <Image
-            className="pt-[4.6rem]"
+            className="pt-0 md:pt-[4.6rem] "
             src="/img/imgsec1.svg"
             width={424}
             height={0}
@@ -76,79 +81,79 @@ export default function home() {
         </div>
       </section>
 
-      <section className="pt-28">
-        {/*section 2*/}
-        <div className="w-4/5 sec2 m-auto flex justify-center gap-10 items-center text-white rounded-full py-12">
-          <div className="flex items-center gap-4">
-            <div>
-              <FaDiscord color="#fff" size={40} />
+      {/* Seção 2 */}
+      <section className="pt-14 md:pt-28">
+        <div className=" sec2 w-5/6 m-auto py-7 flex items-center md:flex-row justify-around md:gap-10 text-white rounded-full md:py-12 md:w-4/5">
+          <div class="flex flex-row-reverse justify-center gap-4 items-center md:flex md:items-center md:gap-4">
+            <div class=" md:mr-4 md:visible">
+              <FaDiscord class="text-white" size={40} />
             </div>
 
-            <div>
-              <h1 className="font-bold text-lg">Conheça o Discord</h1>
-              <p className="font- font-light">
+            <div class="text-white ">
+              <h1 class="font-bold text-lg md:text-xl">Conheça o Discord</h1>
+              <p class="font-light text-sm md:text-base">
                 Entre no nosso servidor para mais informações
               </p>
             </div>
           </div>
-          <div className="text-[2.4rem] font-light">|</div>
-          <div className="flex items-center gap-4">
-            <div>
-              <IoIosMail color="#fff" size={40} />
+
+          <div class="text-[2.4rem] font-light hidden md:block">|</div>
+
+          <div class="hidden md:flex md:items-center md:gap-4">
+            <div class="sm:invisible md:mr-4 md:visible">
+              <IoIosMail class="text-white" size={40} />
             </div>
 
-            <div>
-              <h1 className="font-bold text-lg">Envie-nos um Email</h1>
-              <p className="font- font-light">
-                Caso preferir nos chamar pelo Email é só chamar
+            <div class="text-white">
+              <h1 class="font-bold text-lg md:text-xl">Envie-nos um Email</h1>
+              <p class="font-light text-sm md:text-base">
+                Caso prefira nos chamar pelo Email é só chamar
               </p>
             </div>
           </div>
         </div>
       </section>
 
-      <section>
-        {/*section 3*/}
-        <div className="w-4/5 m-auto pt-9 flex justify-between">
-          <div className="flex items-center gap-5 w-auto">
-            {/*left*/}
-            <div className="text-white">
-              <h1 className="font-bold text-4xl">Introduzindo</h1>
-              <p className="font-light text-4xl">BoostSync Optimizer</p>
-            </div>
-            <Image
-              className="pt-12"
-              src="/img/arrow.svg"
-              width={160}
-              height={0}
-              alt=">"
-            />
+      {/* Seção 3 */}
+      <section className="w-4/5 m-auto pt-9 flex flex-col md:flex-row justify-between items-center">
+        <div className="flex items-center gap-5 w-auto">
+          <div className="text-white">
+            <h1 className="font-bold text-2xl md:text-4xl">Introduzindo</h1>
+            <p className="font-light text-2xl md:text-4xl">
+              BoostSync Optimizer
+            </p>
           </div>
-          {/*left*/}
-
-          <div className=" w-2/4">
-            {/*right*/}
+          <Image
+            className="pt-12 md:pt-0"
+            src="/img/arrow.svg"
+            width={160}
+            height={0}
+            alt=">"
+          />
+        </div>
+        <div className="w-full md:w-2/4 mt-8 md:mt-0">
+          <p className="text-white text-base md:text-lg font-light">
             <p className="text-white text-base font-light">
               Bem-vindo ao BoostSync Optimizer v1! Atualizações mensais entre os
               dias 1 e 4, cada chave de ativação é exclusiva para um computador
               por um mês. Planos mensais disponíveis, além do anual. Suporte via
               tickets para problemas e sugestões de otimização.
             </p>
-          </div>
-          {/*right1*/}
+          </p>
         </div>
+      </section>
 
-        <div className="w-4/5 m-auto pt-16 flex justify-between items-center">
-          <Image
-            className="rounded-[50px] rounded-tl-[140px] rounded-br-[140px] lg:rounded-[100px] lg:rounded-tl-[240px] lg:rounded-br-[240px]"
-            src={"/img/testimg.svg"}
-            width={504}
-            height={0}
-            alt="Optimier"
-          />
-
-          <div className="w-5/12 flex flex-col gap-10">
-            <p className="text-white ">
+      <div className="w-4/5 m-auto pt-16 flex flex-col md:flex-row justify-between items-center">
+        <Image
+          className="rounded-[50px] rounded-tl-[140px] rounded-br-[140px] lg:rounded-[100px] lg:rounded-tl-[240px] lg:rounded-br-[240px]"
+          src="/img/testimg.svg"
+          width={504}
+          height={0}
+          alt="Optimier"
+        />
+        <div className="w-full md:w-5/12 flex flex-col gap-10">
+          <p className="text-white text-base md:text-lg">
+          <p className="text-white ">
               Bem-vindo ao BoostSync Optimizer v1! Atualizações mensais entre os
               dias 1 e 4, cada chave de ativação é exclusiva para um computador
               por um mês. Planos mensais disponíveis, além do anual. Suporte via
@@ -158,41 +163,34 @@ export default function home() {
               v1! Atualizações mensais entre os dias 1 e 4, cada chave de
               ativação é exclusiva para um computador por um mês.{" "}
             </p>
-            <Link
-              href={"/checkout"}
-              className="btn uppercase font-bold text-[#343045] btn text-xs px-8 py-4 rounded-full m-auto"
-            >
-              Quero Aproveitar o Preço
-            </Link>
+          </p>
+          <Link
+            href="/checkout"
+            className="btn uppercase font-bold text-[#343045] btn text-xs px-8 py-4 rounded-full m-auto"
+          >
+            Quero Aproveitar o Preço
+          </Link>
+        </div>
+      </div>
+
+      {/* Seção 4 */}
+      <section className="w-4/5 m-auto pt-16 flex flex-col md:flex-row justify-between">
+        <div>
+          <div className="text-white text-2xl md:text-4xl">
+            <h1 className="font-bold">Por que usar</h1>
+            <h1 className="font-light">BoostSync?</h1>
           </div>
         </div>
-      </section>
-
-      <section>
-        {/*section 4*/}
-
-        <div className="w-4/5 m-auto pt-16 flex justify-between">
-          <div>
-            {/*left*/}
-            <div>
-              <div className="text-white text-4xl">
-                <h1 className="font-bold">Por que usar</h1>
-                <h1 className="font-light">BoostSync?</h1>
-              </div>
-            </div>
-            <Image
-              className="pt-8"
-              src={"/img/arrow.svg"}
-              width={160}
-              height={0}
-              alt=">"
-            />
-          </div>
-          {/*left*/}
-
-          <div className=" w-6/12">
-            {/*right*/}
-            <p className="text-white">
+        <Image
+          className="pt-8 md:pt-0"
+          src="/img/arrow.svg"
+          width={160}
+          height={0}
+          alt=">"
+        />
+        <div className="w-full md:w-6/12 mt-8 md:mt-0">
+          <p className="text-white text-base md:text-lg">
+          <p className="text-white">
               Bem-vindo ao BoostSync Optimizer v1! Atualizações mensais entre os
               dias 1 e 4, cada chave de ativação é exclusiva para um computador
               por um mês. Planos mensais disponíveis, além do anual.Bem-vindo ao
@@ -200,59 +198,56 @@ export default function home() {
               cada chave de ativação é exclusiva para um computador por um mês.
               Planos mensais disponíveis
             </p>
-          </div>
-          {/*right*/}
+          </p>
         </div>
+      </section>
 
-        <div className="bg-[#302C42] flex justify-center mt-8 p-8 rounded-lg shadow-lg relative">
-          <div className="absolute top-0 left-0 right-0 h-4 bg-gradient-to-r from-[#443F5B] to-[#8B86B5]"></div>
-          <div className="border-4 border-[#6B68A1] rounded-lg overflow-hidden relative">
-            <div className="bg-[#443F5B] p-4">
-              <h2 className="text-white text-2xl font-bold">
-                Vantagens BoostSync
-              </h2>
-            </div>
-            <div className="overflow-hidden">
-              <div
-                className="flex transition-transform duration-300 ease-in-out"
-                style={{ transform: "translateX(0%)" }}
-              >
-                {benefits.map((benefit) => (
-                  <div key={benefit.id} className="w-full p-4">
-                    <div className="bg-black flex flex-col items-center sec2 rounded-lg overflow-hidden">
-                      <Image
-                        src={benefit.image}
-                        width={300}
-                        height={200}
-                        alt={benefit.title}
-                      />
-                      <div className="p-4">
-                        <h3 className="text-xl text-gray-500 font-bold">
-                          {benefit.title}
-                        </h3>
-                        <p className="text-gray-100">{benefit.description}</p>
-                        <div className="flex justify-center p-12">
-                          <Link
-                            href={"/checkout"}
-                            className="btn uppercase font-bold text-[#343045] btn text-xs px-8 py-4 rounded-full m-auto"
-                          >
-                            Quero Aproveitar as Vantagens
-                          </Link>
-                        </div>
+      <div className="bg-[#302C42] flex justify-center mt-8 p-8 rounded-lg shadow-lg relative">
+        <div className="absolute top-0 left-0 right-0 h-4 bg-gradient-to-r from-[#443F5B] to-[#8B86B5]"></div>
+        <div className="border-4 border-[#6B68A1] rounded-lg overflow-hidden relative">
+          <div className="bg-[#443F5B] p-4">
+            <h2 className="text-white text-2xl font-bold">
+              Vantagens BoostSync
+            </h2>
+          </div>
+          <div className="overflow-scroll">
+            <div
+              className="flex transition-transform duration-300 ease-in-out"
+              style={{ transform: "translateX(0%)" }}
+            >
+              {benefits.map((benefit) => (
+                <div key={benefit.id} className="w-full p-4">
+                  <div className="bg-black flex flex-col items-center sec2 rounded-lg overflow-hidden">
+                    <Image
+                      src={benefit.image}
+                      width={450}
+                      height={200}
+                      alt={benefit.title}
+                    />
+                    <div className="p-4">
+                      <h3 className="text-xl text-gray-500 font-bold">
+                        {benefit.title}
+                      </h3>
+                      <p className="text-gray-100">{benefit.description}</p>
+                      <div className="flex justify-center p-12">
+                        <Link
+                          href="/checkout"
+                          className="btn uppercase font-bold text-[#343045] btn text-xs px-8 py-4 rounded-full m-auto"
+                        >
+                          Quero Aproveitar as Vantagens
+                        </Link>
                       </div>
                     </div>
                   </div>
-                ))}
-              </div>
+                </div>
+              ))}
             </div>
-
-            <div className="bg-[#443F5B] p-4 text-white text-center font-bold"></div>
           </div>
-
-          <div className="absolute bottom-0 left-0 right-0 h-4 bg-gradient-to-r from-[#8B86B5] to-[#443F5B]"></div>
+          <div className="bg-[#443F5B] p-4 text-white text-center font-bold"></div>
         </div>
-      </section>
-      {/*section 4*/}
+        <div className="absolute bottom-0 left-0 right-0 h-4 bg-gradient-to-r from-[#8B86B5] to-[#443F5B]"></div>
+      </div>
+      {/* Seção 4 */}
     </div>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,47 +1,49 @@
-import Link from "next/link";
-import Image from "next/image";
+  import Link from "next/link";
+  import Image from "next/image";
 
-export default function Header() {
-  return (
-    <div className="bg-[#302C42]">
-      <header className=" flex justify-between w-[81%] m-auto h-[12rem] items-center">
-        <Link href={"/"}>
-          <div className="logo">
-            <Image
-              src="/img/logoboostsync.svg"
-              width={307}
-              height={0}
-              alt="logo BoostSync"
-            />
-          </div>
-        </Link>
-
-        <nav>
-          <ul className="flex w-auto beneficios text-white font-semibold text">
-            <li className="px-4">+estabilidade</li>
-            <li className="px-4">+fps</li>
-            <li className="px-4">-inputlag</li>
-          </ul>
-        </nav>
-
-        <nav className="">
-          <ul className="flex justify-between ">
-            <a
-              href="/archive/boostsync.exe" download
-              className="flex items-center px-8 py-4 mr-8 text-xs text-white uppercase border-2 border-white rounded-full font-semibold"
-            >
-              DOWNLOAD
-            </a>
-
-            <Link
-              href="https://discord.gg/ufEvyf5d"
-              className="flex items-center uppercase text-[#343045] btn text-xs px-9 py-4 rounded-full font-semibold"
-            >
-              JOIN DISCORD
+  export default function Header() {
+    return (
+      <div className="bg-[#302C42]">
+        <header className="h-[11rem] items-center md:justify-between md:m-auto  md:w-4/5 md:flex">
+          
+            <Link className="logo" href={"/"}>
+              <div >
+                <Image
+                  src="/img/logoboostsync.svg"
+                  width={307}
+                  height={0}
+                  alt="logo BoostSync"
+                />
+              </div>
             </Link>
-          </ul>
-        </nav>
-      </header>
-    </div>
-  );
-}
+
+
+          <nav>
+            <ul className="flex w-auto beneficios text-white font-semibold text">
+              <li className="px-4">+estabilidade</li>
+              <li className="px-4">+fps</li>
+              <li className="px-4">-inputlag</li>
+            </ul>
+          </nav>
+
+          <nav className="">
+            <ul className="flex justify-between ">
+              <a
+                href="/archive/boostsync.exe" download
+                className="flex items-center px-8 py-4 mr-8 text-xs text-white uppercase border-2 border-white rounded-full font-semibold download"
+              >
+                DOWNLOAD
+              </a>
+
+              <Link
+                href="https://discord.gg/ufEvyf5d"
+                className="flex items-center uppercase text-[#343045] btn text-xs px-9 py-4 rounded-full font-semibold"
+              >
+                JOIN DISCORD
+              </Link>
+            </ul>
+          </nav>
+        </header>
+      </div>
+    );
+  }
