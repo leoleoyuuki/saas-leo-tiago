a6d40b17cef3566a1419f5240fc2a71af130b297
leoleoyuuki <rm550373@fiap.com.br>
2024-02-20 20:18:14 -0300

    Refactor checkout page and update responsive styles

--- src/app/checkout/page.jsx
+++ src/app/checkout/page.jsx
@@ -1,7 +1,6 @@
 "use client"
 
-import Image from "next/image";
-import Link from "next/link";
+
 import {LazyLoadImage} from 'react-lazy-load-image-component';
 
 
@@ -17,17 +16,18 @@ export default function Checkout () {
       <div className="bg-[#26242f] pb-10">
       <section className="w-4/5 m-auto">
         <div className="container-sec1 flex justify-between">
-          <div className="leftsec1">
-            <div className="texts text-4xl font-semibold pt-10">
+          <div>
+            <div className="texts text-4xl flex flex-col font-semibold pt-10">
               <h1>
-                <span className="t1">Adquira</span> a Sua Licença Abaixo
+                <span className="t1">Adquira</span> a Sua Licença
               </h1>
+              <p className="text-[0.6rem] font-light">clique na imagem abaixo</p>
             </div>
           </div>
         </div>
       </section>
 
-      <section className="w-4/5 m-auto pt-16 flex items-center justify-center md:flex-row">
+      <section className="w-4/5 m-auto pt-10 flex items-center justify-center">
         <div className="cursor-pointer hover:scale-105 hover:border-2 rounded-2xl border-purple-500" onClick={handleCheckout}>
             <LazyLoadImage
               className="rounded-2xl"
